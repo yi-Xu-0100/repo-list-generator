0b747b31b0176d0661805c629785b4c94909b831
yi_Xu <xjy123456120@163.com>
2020-10-11 17:33:34 +0800

    🎉 first test

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const src = path.join(__dirname, '..');
 async function run() {
   core.startGroup('Get Input value');
   const username = core.getInput('user', { require: false });
+  core.info(`src: ${src}`);
   core.info(`username: ${username}`);
   core.endGroup();
 }
