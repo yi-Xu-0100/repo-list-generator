d23966935278a5c7215f5a52d1c984b2ec4fc3d7
yi_Xu <xjy123456120@163.com>
2020-10-16 11:05:19 +0800

    🔥 delete some invalid output

--- src/repo.js
+++ src/repo.js
@@ -3,6 +3,8 @@ const { debug, info, getInput, setOutput } = require('@actions/core');
 const my_token = getInput('my_token', { require: true });
 const octokit = new getOctokit(my_token);
 const { pluck, zip, unzip, reject } = require('underscore');
+const { join } = require('path');
+const { writeFileSync } = require('fs');
 
 let getAll = async function (page = 10) {
   var repo_list = [];
@@ -18,10 +20,18 @@ let getAll = async function (page = 10) {
       throw err;
     }
   }
+
+  debug(`repo-info: ${join(__dirname, '../.repo_list/repo-info.json')}`);
+  writeFileSync(
+    join(__dirname, '../.repo_list/repo-info.json'),
+    JSON.stringify(repo_list, null, 2),
+    'utf-8'
+  );
+
   var repo_list_name = pluck(repo_list, 'name');
   var repo_list_private = pluck(repo_list, 'private');
   var repo_list_fork = pluck(repo_list, 'fork');
-  info('Successfully get repo data.');
+  info('[Info]: Successfully get repo data.');
   return { repo_list: zip(repo_list_name, repo_list_private, repo_list_fork) };
 };
 
@@ -29,6 +39,11 @@ let getList = async function (repo_list) {
   debug(`repo_list:`);
   debug(JSON.stringify(repo_list));
   var repos = repo_list.repo_list;
+  const repos_name_ALL = unzip(repos)[0];
+
+  const repoList = unzip(reject(repos, item => item[1] || item[2]))[0];
+  debug(`repoList[${repoList.length}]: ${repoList.toString()}`);
+  setOutput('repoList', repoList.toString());
 
   const repoList_ALL = unzip(repos)[0];
   debug(`repoList_ALL[${repoList_ALL.length}]: ${repoList_ALL.toString()}`);
@@ -42,33 +57,27 @@ let getList = async function (repo_list) {
   debug(`repoList_FORK[${repoList_FORK.length}]: ${repoList_FORK.toString()}`);
   setOutput('repoList_FORK', repoList_FORK.toString());
 
-  const repoList = unzip(reject(repos, item => item[1] || item[2]))[0];
-  debug(`repoList[${repoList.length}]: ${repoList.toString()}`);
-  setOutput('repoList', repoList.toString());
-
-  const privateList_ALL = unzip(reject(repos, item => !item[1]))[0];
-  debug(`privateList_ALL[${privateList_ALL.length}]: ${privateList_ALL.toString()}`);
-  setOutput('privateList_ALL', privateList_ALL.toString());
-
   const privateList = unzip(reject(repos, item => !item[1] || item[2]))[0];
   debug(`privateList[${privateList.length}]: ${privateList.toString()}`);
   setOutput('privateList', privateList.toString());
 
-  const forkList_ALL = unzip(reject(repos, item => !item[2]))[0];
-  debug(`forkList_ALL[${forkList_ALL.length}]: ${forkList_ALL.toString()}`);
-  setOutput('forkList_ALL', forkList_ALL.toString());
-
   const forkList = unzip(reject(repos, item => !item[2] || item[1]))[0];
   debug(`forkList[${forkList.length}]: ${forkList.toString()}`);
   setOutput('forkList', forkList.toString());
+
+  info(`[Info]: repos_name_ALL ${repos_name_ALL.length}`);
+  info(`[Info]: repoList ${repoList.length}`);
+  info(`[Info]: repoList_ALL ${repoList_ALL.length}`);
+  info(`[Info]: repoList_PRIVATE ${repoList_PRIVATE.length}`);
+  info(`[Info]: repoList_FORK ${repoList_FORK.length}`);
+  info(`[Info]: privateList ${repoList_PRIVATE.length}`);
+  info(`[Info]: forkList ${repoList_FORK.length}`);
   return {
+    repoList: repoList,
     repoList_ALL: repoList_ALL,
     repoList_PRIVATE: repoList_PRIVATE,
     repoList_FORK: repoList_FORK,
-    repoList: repoList,
-    privateList_ALL: privateList_ALL,
     privateList: privateList,
-    forkList_ALL: forkList_ALL,
     forkList: forkList
   };
 };
